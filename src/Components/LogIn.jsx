1a780a96d720e22cc8742ef034ac5c4d386f7912
johnbonanno-source <john.bon991@gmail.com>
2023-08-13 13:57:14 -0600

    remove unused imports and create balance box component

--- src/Components/LogIn.jsx
+++ src/Components/LogIn.jsx
@@ -1,78 +1,87 @@
-import React, { useState } from 'react';
-import App from '../App.js'
-import LoginButton from './LoginButton.jsx';
-import classes from './LoginButton.module.css';
+import React, { useState } from "react";
+import LoginButton from "./LoginButton.jsx";
+import classes from "./LoginButton.module.css";
 // import inputStyle from './LogIn.module.css';
-import axios from 'axios';
+import axios from "axios";
 
-function Login({loggedIn,setLoggedIn}) {
+function Login({ loggedIn, setLoggedIn }) {
+  function handleLogout() {
+    setLoggedIn(false);
+  }
 
-    function handleLogout() {
-        setLoggedIn(false);
-    };
- 
-    function handleLogin(){
+  function handleLogin() {
+    const passwordInput = document.getElementById("password");
+    const usernameInput = document.getElementById("username");
 
-        const passwordInput = document.getElementById('password');
-        const usernameInput = document.getElementById('username');
-      
-        if (passwordInput && usernameInput) {
-          const passwordValue = passwordInput.value;
-          const usernameValue = usernameInput.value;
+    if (passwordInput && usernameInput) {
+      const passwordValue = passwordInput.value;
+      const usernameValue = usernameInput.value;
 
-            let url = `http://localhost:5134/Users/username/${usernameValue}`;
-            axios.get(url).then(response => {
-                    if (response.data.username === usernameValue && response.data.password===passwordValue)
-                    {
-                        setLoggedIn(true);
-                    }
-                    else{
-                    }
-            }).catch(error => {
-                const mockUserData = require('../Mocks/mockUserData.json'); // Load mock data from JSON file
-                console.error('Backend not reachable:', error);
-                const mockUser = mockUserData.find(user => user.username === usernameValue);
-                if(mockUser && mockUser.password === passwordValue){
-                    setLoggedIn(true);
-                }
-            });
-        }
+      let url = `http://localhost:5134/Users/username/${usernameValue}`;
+      axios
+        .get(url)
+        .then((response) => {
+          if (
+            response.data.username === usernameValue &&
+            response.data.password === passwordValue
+          ) {
+            setLoggedIn(true);
+          } else {
+          }
+        })
+        .catch((error) => {
+          const mockUserData = require("../Mocks/mockUserData.json"); // Load mock data from JSON file
+          console.error("Backend not reachable:", error);
+          const mockUser = mockUserData.find(
+            (user) => user.username === usernameValue
+          );
+          if (mockUser && mockUser.password === passwordValue) {
+            setLoggedIn(true);
+          }
+        });
     }
+  }
 
+  return (
+    <>
+      <div>
+        {!loggedIn ? (
+          <>
+            <input
+              className={classes.inputStyle}
+              id={"username"}
+              type="text"
+              placeholder="Username"
+            />
 
-    return (
-        <>
-            <div>
-                {
-                    !loggedIn ? 
-                    (
-                        <>
-                            <input 
-                            className={classes.inputStyle}
-                            id={"username"} 
-                            type="text" 
-                            placeholder="Username"
-                            />
-
-                            <input 
-                            className={classes.inputStyle}
-                            id={"password"}  
-                            type="password" 
-                            placeholder="Password"
-                            />
-                            <LoginButton className={classes.logOut} buttonText={"Login"} onClick={handleLogin}>Login</LoginButton>
-                        </>
-                    )
-                    :
-                    (
-                        <>
-                            <LoginButton className={classes.logOut} buttonText={"Logout"} onClick={handleLogout}>Logout</LoginButton>
-                        </>
-                    )
-                }
-            </div>
-        </>
-    );     
+            <input
+              className={classes.inputStyle}
+              id={"password"}
+              type="password"
+              placeholder="Password"
+            />
+            <LoginButton
+              className={classes.logOut}
+              buttonText={"Login"}
+              onClick={handleLogin}
+            >
+              Login
+            </LoginButton>
+          </>
+        ) : (
+          <>
+            <LoginButton
+              className={classes.logOut}
+              buttonText={"Logout"}
+              onClick={handleLogout}
+            >
+              Logout
+            </LoginButton>
+          </>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Login;
--- src/Components/MainHeader.jsx
+++ src/Components/MainHeader.jsx
@@ -1,22 +1,25 @@
-import { MdPostAdd, MdMessage, MdAccountCircle } from 'react-icons/md';
-import App from '../App';
-import classes from './MainHeader.module.css';
-import { BsFillEmojiSmileUpsideDownFill, BsCreditCard2Back } from "react-icons/bs";
+import classes from "./MainHeader.module.css";
+import {
+  BsFillEmojiSmileUpsideDownFill,
+  BsCreditCard2Back,
+} from "react-icons/bs";
 import { AiOutlineMenu } from "react-icons/ai";
-import Login from './LogIn'
+import Login from "./LogIn";
 
-function MainHeader({ loggedIn,setLoggedIn }) {
+function MainHeader({ loggedIn, setLoggedIn }) {
   return (
     <header className={classes.header}>
       <h1 className={classes.logo}>
-        BudgetHero <BsCreditCard2Back/>
+        BudgetHero <BsCreditCard2Back />
       </h1>
-        <div className={classes.navComponentBox}>
-          <button className={classes.navButton}>Home</button>
-          <button className={classes.navButton}>Budget</button>
-          <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
-         <button className={classes.navButton}><AiOutlineMenu className={classes.menuIcon}/></button>
-        </div>
+      <div className={classes.navComponentBox}>
+        <button className={classes.navButton}>Home</button>
+        <button className={classes.navButton}>Budget</button>
+        <Login loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+        <button className={classes.navButton}>
+          <AiOutlineMenu className={classes.menuIcon} />
+        </button>
+      </div>
     </header>
   );
 }
