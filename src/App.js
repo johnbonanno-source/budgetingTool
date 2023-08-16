f0d2ca7c6597177b812752e99d91ff1928311b92
john <john.bon991@gmail.com>
2023-08-16 13:14:00 -0600

    move css styles out from .app .css into app.module.css

--- src/App.js
+++ src/App.js
@@ -4,30 +4,12 @@ import { useState } from 'react';
 import MainHeader from './Components/MainHeader';
 import PageFooter from './Components/PageFooter';
 import AvailableBalance from './Components/AvailableBalance';
+import classes from './App.module.css';
 
 function App() {
   const [loggedIn,setLoggedIn] = useState(false);
   const [balance, setBalance] = useState(10);
   
-
-  const divStyle = {
-    visibility: loggedIn ? 'visible' : 'hidden',
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    gap:"1rem"
-  };
-
-  const imgStyle = {
-    maxWidth: "100%",
-    marginBottom:"1rem",
-
-  };
-
-  const loggedoutStyle = {
-    visibility: loggedIn ? 'hidden' : 'visible', 
-  };
-
   const addLoginHandler = async(username,password)=>{
     const response = await fetch(`http://localhost:2000/login?username=${username}`, {
       method: 'GET',
@@ -48,15 +30,23 @@ function App() {
 
   return <>
     <MainHeader isLoggedIn={loggedIn} setLoggedIn={setLoggedIn} onLogin={addLoginHandler}/>
-    <div style={divStyle}>
+    {loggedIn ? (
+    <div className={classes.divStyle}>
       <AvailableBalance balance={balance} setBalance={setBalance}/>
       <img
                 src="https://gdm-catalog-fmapi-prod.imgix.net/ProductScreenshot/87539ba2-07d6-4642-9788-21a631b6f4c1.png"
                 alt="W3Schools.com"
-                style={imgStyle} // Adjust the image width as needed
+                // Adjust the image width as needed
             />
+    </div>):(
+      <div>
+        <img
+          src="https://gdm-catalog-fmapi-prod.imgix.net/ProductScreenshot/87539ba2-07d6-4642-9788-21a631b6f4c1.png"
+          alt="W3Schools.com"
+        />
+        <PageFooter/>
     </div>
-    <PageFooter/>
+    )};
   </>
   
 }
--- src/Components/PageFooter.jsx
+++ src/Components/PageFooter.jsx
@@ -7,13 +7,13 @@ function PageFooter() {
         <div className={classes.footerList}>
           <p>Employment</p>
           <p>Contact</p>
-          <p>Drug Abuse</p>
+          <p>rug Abuse</p>
           <p>Womanizer</p>
         </div>
         <div className={classes.footerList}>
           <p>Employment</p>
           <p>Contact</p>
-          <p>Drug Abuse</p>
+          <p>rug Abuse</p>
           <p>Womanizer</p>
         </div>
       </div>
