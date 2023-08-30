a6616498243a042aba5ff96936f90216a76dd05f
johnbonanno-source <john.bon991@gmail.com>
2023-08-29 19:38:21 -0600

    dynamics

--- src/Pages/Login and Registration Pages/LoginPage.jsx
+++ src/Pages/Login and Registration Pages/LoginPage.jsx
@@ -5,7 +5,6 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import LCC from "../../Assets/LCC.jpg";
 import Wrapper from "../../Components/Wrapper/Wrapper";
-
 const LoginPage = () => {
   const navigate = useNavigate();
 
@@ -39,8 +38,63 @@ const LoginPage = () => {
     }
   };
 
+  const [zoomLevel, setZoomLevel] = useState(1); // Default zoom level
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setZoomLevel(window.devicePixelRatio);
+    };
+
+    window.addEventListener("resize", handleResize);
+    handleResize(); 
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   return (
     <>
+      {zoomLevel > 2 ? (
+        <Wrapper>
+        <img src={LCC} alt="pic" />
+        <div className={classes.loginBoxStyles2}>
+          <h4 className={classes.h4}>Login</h4>
+          <form onSubmit={handleLogin}>
+            <input
+              className={classes.logininput2}
+              id="username"
+              type="text"
+              value={credentials.username}
+              placeholder="Username"
+              name="username"
+              onChange={handleChange}
+            />
+            <input
+              className={classes.logininput2}
+              id="password"
+              type="password"
+              value={credentials.password}
+              placeholder="Password"
+              name="password"
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className={classes.loginBtn2}
+              onClick={handleLogin}
+            >
+              Login
+            </button>
+          </form>
+          <Link to="/register" className={classes.linkStyle2}>
+            Sign Up
+          </Link>
+        </div>
+      </Wrapper>
+      ) : (
       <Wrapper>
         <img src={LCC} alt="pic" />
         <div className={classes.loginBoxStyles}>
@@ -76,7 +130,7 @@ const LoginPage = () => {
             Sign Up
           </Link>
         </div>
-      </Wrapper>
+      </Wrapper>)}
     </>
   );
 };
