08e9e0816844ebd082de1796d795cc713ffda670
johnbonanno-source <john.bon991@gmail.com>
2023-09-04 20:06:00 -0600

    navbar button group"

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,20 +1,45 @@
-import classes from './Footer.module.css';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef } from 'react';
+import { AppBar, Toolbar, ButtonGroup, Button, Box } from '@mui/material';
 
 function Footer() {
+  const appBarRef = useRef(null);
+
+  useEffect(() => {
+    const buttonsHeight = appBarRef.current.querySelector('.MuiButton-contained').clientHeight;
+    appBarRef.current.style.minHeight = `${buttonsHeight}px`;
+  }, []);
+
   return (
-      <footer className={classes.pageFooter}>
-        <div className={classes.footerContainer}>
-          <div className={classes.footerList}>
-            <Link className={classes.linkStyle}>Employment</Link>
-            <Link className={classes.linkStyle}>Contact</Link>
-            <Link className={classes.linkStyle}>rug Abuse</Link>
-            <Link className={classes.linkStyle}>Womanizer</Link>
-          </div>
-          
-        </div>
-      </footer>
-    
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="fixed" style={{ top: "auto", bottom: 0 }} ref={appBarRef}>
+        <Toolbar>
+          <ButtonGroup
+            variant="contained"
+            aria-label="navbar button group"
+            sx={{ justifyContent: "auto" }}
+          >
+            <Button
+              href="./home"
+              sx={{ "&:hover": { border: "1px solid white" } }}
+            >
+              Home
+            </Button>
+            <Button href="./budget" sx={{ "&:hover": { border: "none" } }}>
+              Budget
+            </Button>
+            <Button
+              href="./home"
+              sx={{ "&:hover": { border: "1px solid white" } }}
+            >
+              Home
+            </Button>
+            <Button href="./budget" sx={{ "&:hover": { border: "none" } }}>
+              Budget
+            </Button>
+          </ButtonGroup>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import addLogoutHandler from '../../Api/LogoutApi';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import IconButton from '@mui/material/IconButton';
+
 import MenuIcon from '@mui/icons-material/Menu';
 import { borders } from '@mui/system';
+import { AppBar, Box, Toolbar, Typography, Button, ButtonGroup, IconButton } from '@mui/material';
+
 
 
 import { BsCreditCard2Back } from 'react-icons/bs';
@@ -39,20 +35,11 @@ const NavBar = () => {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position='static'>
         <Toolbar>
-          {/* <IconButton
-            size='large'
-            edge='start'
-            color='inherit'
-            aria-label='menu'
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton> */}
           <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
             BudgetHero <BsCreditCard2Back className={classes.logo} />
           </Typography>
           <ButtonGroup variant='contained' aria-label='navbar button group'>
-            <Button href='./home' sx={{ '&:hover': { border: '1px solid white' } }}>
+            <Button href='./' sx={{ '&:hover': { border: '1px solid white' } }}>
               Home
             </Button>
             <Button href='./budget' sx={{ '&:hover': { border: 'none' } }}>
