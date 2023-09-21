3dc19e36529e6e99b09e1e8f304671704e3c426c
johnbonanno-source <john.bon991@gmail.com>
2023-09-21 08:57:41 -0600

    fixed post to logout

--- src/Api/SessionTokenApi.js
+++ src/Api/SessionTokenApi.js
@@ -4,15 +4,19 @@ const apiBaseUrl = 'http://localhost:2001';
 
 const addLoginHandler = async (username, password) => {
   try {
-    const response = await axios.post(`${apiBaseUrl}/login`, {
-      username,
-      password,
-    }, {
-      headers: {
-        'Content-Type': 'application/json',
+    const response = await axios.post(
+      `${apiBaseUrl}/login`,
+      {
+        username,
+        password,
       },
-      withCredentials: true,
-    });
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      }
+    );
 
     if (response.status !== 200) {
       throw new Error('Login failed');
@@ -25,25 +29,27 @@ const addLoginHandler = async (username, password) => {
   }
 };
 
-
 const addLogoutHandler = async () => {
-    try {
-      const response = await fetch('http://localhost:2001/logout', {
-        method: 'POST',
+  try {
+    const response = await axios.post(
+      `${apiBaseUrl}/logout`,
+      {},
+      {
         headers: {
           'Content-Type': 'application/json',
         },
-      });
-      if (!response.ok) {
-        throw new Error('Logout failed');
+        withCredentials: true,
       }
-      const responseData = await response.json();
-  
-      return responseData;
-    } catch (error) {
-      console.error('Login error:', error.message);
+    );
+    if (!response.ok) {
+      throw new Error('Logout failed');
     }
-  };
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.error('logout error:', error.message);
+  }
+};
 
 const getSessionTokenById = async () => {
   try {
@@ -51,16 +57,17 @@ const getSessionTokenById = async () => {
       headers: {
         'Content-Type': 'application/json',
       },
+      withCredentials: true,
     });
 
-    if (response.status !== 200) {
+    if (!response.ok) {
       throw new Error('Unable to fetch user id from session token.');
     }
 
-    return response.data.userId; 
+    return response.data.userId;
   } catch (error) {
     console.error('Session token error: ', error.message);
   }
 };
-  
-  export { addLoginHandler, getSessionTokenById, addLogoutHandler };
+
+export { addLoginHandler, getSessionTokenById, addLogoutHandler };
